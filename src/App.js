7acfa0f3835bccdac2af202b961f8eb628d0b383
Esteban <silvaesteban7726@gmail.com>
2022-03-02 23:55:31 -0300

    feat: :construction: Empezando con el generador de memes
    - Se agrego el selector para poder elegir la imagen del meme.
    - Se agregaron los inputs para escribir las 2 frases del meme.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
 import './App.css';
 
 function App() {
+
+  const [linea1, setLinea1] = useState("");
+  const [linea2, setLinea2] = useState("");
+
+  const onChangeLinea1 = evento => {
+    setLinea1(evento.target.value);
+  }
+
+  const onChangeLinea2 = evento => {
+    setLinea2(evento.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <select>
+        <option value="fire">Casa en llamas</option>
+        <option value="futurama">Futurama</option>
+        <option value="history">History</option>
+        <option value="matrix">Matrix</option>
+        <option value="philosoraptor">Philosoraptor</option>
+        <option value="smart">Smart Guy</option>
+      </select> <br />
+      
+      <input onChange={onChangeLinea1} type="text" placeholder="Línea 1"></input> <br />
+      <input onChange={onChangeLinea2} type="text" placeholder="Línea 2"></input> <br />
+
+      <button>Exportar</button>
+
+      <div>
+        <span>{linea1}</span> <br />
+        <span>{linea2}</span>
+        <img src="" />
+      </div>
+
     </div>
   );
 }
